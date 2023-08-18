5ff0295afbb309ddad46d6673ce656da2b8c2801
Selmantopal97 <setpa97@hotmail.com>
2023-08-18 17:51:03 +0300

    Revert "ff"
    This reverts commit a27d306fbab466e62bbcc0809e16b558e24f9f19.

---
+++ promise.js
@@ -0,0 +1,30 @@
+const books = [
+    {name: 'kitap1', yazar: 'yazar1'},
+    {name: 'kitap2', yazar: 'yazar2'},
+    {name: 'kitap3', yazar: 'yazar3'}
+    ];
+
+const listBooks = () => {
+    books.map(book => {
+        console.log(book.name);
+    });
+};
+
+// listBooks();
+
+const addBook = (newBook) => {
+
+    const promise1=new Promise((resolve,reject) => {
+        books.push(newBook);
+        resolve(books);
+        reject('hata');
+    })
+        return promise1;
+};
+
+addBook({name: 'kitap5', yazar: 'yazar5'}).then(() => {
+        console.log('Yeni Liste');
+        listBooks();
+    }).catch((error) => {
+        console.log(error);
+    });
